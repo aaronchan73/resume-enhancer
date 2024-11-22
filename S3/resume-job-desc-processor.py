286ec886cf2fa85597f8a9b42d1fa5bb0ee74a92
Aaron Chan <chan.aaron73@gmail.com>
2024-11-21 17:09:08 -0800

    fix lambda to call ec2 endpoint + dynamodb query

--- S3/resume-job-desc-processor.py
+++ S3/resume-job-desc-processor.py
@@ -1,9 +1,18 @@
 import json
 from collections import Counter
 import boto3
+import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+EC2_URL = os.getenv("EC2_URL")
 
 def lambda_handler(event, context):
+    def parse_uuid(file_key):
+        return file_key.split("_")[0]
+
     try:
         s3 = boto3.client("s3")
 
@@ -20,13 +29,15 @@ def lambda_handler(event, context):
 
         print(f"Content of the file {file_key} from bucket {bucket_name}:")
 
-        resume_id = ""
+        resume_id = parse_uuid(file_key)
         input_resume = ""
         input_job_desc = ""
 
-        # TODO: Call REST API on the ec2 server and get the returned enhanced resume
-
         enhanced_resume = ""
+        ec2_endpoint = EC2_URL + "/enhance/" + bucket_name + "/" + resume_id
+
+        response = requests.get(ec2_endpoint)
+        enhanced_resume = response.json()["body"]
 
         resume_table = boto3.resource("dynamodb", region_name="ca-central-1").Table(
             "enhanced_resumes"
@@ -37,8 +48,10 @@ def lambda_handler(event, context):
             "input_job_desc": input_job_desc,
             "enhanced_resume": enhanced_resume,
         }
+        resume_table.put_item(Item=data)
+       
     except Exception as e:
         print(f"Exception: {e}")
         return {"statusCode": 500, "body": json.dumps({"message ": str(e)})}
 
-    return {"statusCode": 200, "body": str(sorted_word_count)}
+    return {"statusCode": 200, "body": data}
